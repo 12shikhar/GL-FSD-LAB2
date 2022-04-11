9e14fbbb6cc7f790ddb596b860b698a912abcca1
12shikhar <99669077+12shikhar@users.noreply.github.com>
2022-04-11 20:35:40 +0530

    Add files via upload

---
+++ src/com/gl/javafsd/paymoney/transaction/Main.java
@@ -0,0 +1,11 @@
+package com.gl.javafsd.paymoney.transaction;
+
+	public class Main {
+
+		public static void main(String[] args) {
+			
+			PaymoneyTransactionUI ui = new PaymoneyTransactionUI();
+		ui.init();		
+		}
+	}
+
